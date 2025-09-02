f51c90152fab02dd0db623a4d6b1a84228095baf
aumgogo78 <aumzzzlkii@gmail.com>
2025-09-02 09:42:07 +0700

    first commit

---
+++ components/header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+
+<nav class="bg-light p-3 vh-100">
+    <div class="d-flex container" style="justify-content: space-between; padding: 25px;">
+    <a href="#" class="nav-link text-dark"><i class="bi bi-person-fill"></i>ACCOUNT</a>
+    <a href="#" class="nav-link text-dark"><i class="bi bi-basket2-fill"></i>BASKET</a>
+    </div>
+    <h4 class="text-center mb-4 taviraj-thin color-text" style="font-size: 90px;">HOMEY</h4>
+    <div class="line">
+        <ul class="nav nav-link d-flex py-3" style="justify-content: center; gap: 90px;">
+            <li><a class="color-menu nav nav-link px-2 py-0" href="">Home</a></li>
+            <li><a class="color-menu nav nav-link px-2 py-0" href="">Menu</a></li>
+            <li><a class="color-menu nav nav-link px-2 py-0" href="">Order</a></li>
+            <li><a class="color-menu nav nav-link px-2 py-0" href="">Contact Us</a></li>
+        </ul>
+    </div>
+</nav>
